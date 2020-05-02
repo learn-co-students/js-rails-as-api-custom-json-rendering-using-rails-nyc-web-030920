dded371c15e83b696b1a523144540807fcd77cda
github username <github email address>
2020-05-02 15:25:29 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  get '/birds' => 'birds#index'
+  # get '/birds' => 'birds#index'
+  # get '/birds' => 'birds#show'
+
+  resources :birds, only: [:show, :index]
 end
